d53dc9d6be9595fb730f21aed088b2eb6e7321ff
Saina <sainakey5@gmail.com>
2023-12-07 00:58:59 +0900

    Vueで遊んだ

---
+++ src/components/slider.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ msg: string }>()
+
+const sliderValue = ref(0)
+
+</script>
+
+<template>
+  <div>
+    <p>こんにちは～</p>
+      <form>
+        <input type="range" className="slider" v-model="sliderValue" min="0" max="255">
+      </form>
+      {{sliderValue}}1
+  </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
---
+++ src/components/sliderColor.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { ref, computed } from 'vue'
+
+defineProps<{ msg: string }>()
+
+const sliderValue = ref(0)
+
+const randomColor = computed(() => {
+  const hue = 60
+  const saturation = Math.floor(Math.random() * 100)
+  const lightness = Math.floor(Math.random() * 100)
+  return `hsl(${hue}, ${saturation}%, ${lightness}%)`
+})
+</script>
+
+<template>
+  <div :style="{ background: randomColor }">
+    <p>マウスでスライドしてみよう</p>
+    <form>
+      <input type="range" className="slider" v-model="sliderValue" min="0" max="255">
+    </form>
+    {{ sliderValue }}
+  </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
